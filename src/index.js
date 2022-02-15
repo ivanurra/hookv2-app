af6b034ceb38efbaa82d1c924f746f0315537cd3
Ivan Urra <ivan.urra.wd@gmail.com>
2022-02-15 18:50:26 +0100

    Hookv2-app WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { SimpleForm } from "./components/useEffect/SimpleForm";
-// import { Countdown } from "./components/useState/Countdown";
-// import { CustomHook } from "./components/useState/CustomHook";
 
 ReactDOM.render(<SimpleForm />, document.getElementById("root"));
