b747830d0aeee6f67ce0a03b17e56c9097c79718
Ivan Urra <ivan.urra.wd@gmail.com>
2022-02-21 19:08:02 +0100

    WIP

---
+++ src/components/useState/Form.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useForm } from "../../hooks/useForm";
+import "./styles.scss";
+
+export const Form = () => {
+  const [formValues, handleInputChange] = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { name, email, password } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formValues);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Login 🚀</h1>
+      <hr />
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Name"
+          autoComplete="off"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          placeholder="Email"
+          autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="Password"
+          autoComplete="off"
+          value={password}
+          onChange={handleInputChange}
+        />
+      </div>
+      {name && email && password && (
+        <button className="btn-primary btn" type="submit">
+          Send
+        </button>
+      )}
+    </form>
+  );
+};
