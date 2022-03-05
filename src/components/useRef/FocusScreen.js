c4997e71e4e25b1b8408ed52a4ba27644a7a6460
Ivan Urra <ivan.urra.wd@gmail.com>
2022-03-05 13:39:10 +0100

    useRef

--- src/components/useRef/FocusScreen.js
+++ src/components/useRef/FocusScreen.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../useEffect/effects.scss";
 
 export const FocusScreen = () => {
+
+  const inputRef = useRef();
+
+  const handleClick = () => {
+    inputRef.current.select();
+  };
+
   return (
     <div>
       <h1>FocusScreen</h1>
       <hr />
-      <input className="form-control" placeholder="Your name"/>
-      <button className="mt-2 btn btn-outline-secondary">Focus</button>
+      <input className="form-control" placeholder="Your name" ref={inputRef}/>
+      <button className="mt-2 btn btn-outline-secondary" onClick={handleClick}>
+        Focus
+      </button>
     </div>
   );
 };
